5d923a0597b604107f7a9330498790137adddaed
Lee Shuman <jls.engr@gmail.com>
2019-01-31 22:36:45 -0600

    Created folder structure, updated settings, added gladiator requirements

--- MAD Game Collection/src/app/app-routing.module.ts
+++ MAD Game Collection/src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
     { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
-    { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
-    { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
-    { path: "featured", loadChildren: "~/app/featured/featured.module#FeaturedModule" },
-    { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" }
+    { path: "home", loadChildren: "~/app/pages/home/home.module#HomeModule" },
+    { path: "browse", loadChildren: "~/app/pages/browse/browse.module#BrowseModule" },
+    { path: "search", loadChildren: "~/app/pages/search/search.module#SearchModule" },
+    { path: "featured", loadChildren: "~/app/pages/featured/featured.module#FeaturedModule" },
+    { path: "settings", loadChildren: "~/app/pages/settings/settings.module#SettingsModule" }
 ];
 
 @NgModule({
