c99fc3df4242667e70dbe6a4bdd4b39a40c75393
Lee Shuman <jls.engr@gmail.com>
2019-02-22 13:20:21 -0600

    Designed basic leaderboard. Does not request/update scores.

--- MAD Game Collection/src/app/pages/pageNotFound/pageNotFound.component.ts
+++ MAD Game Collection/src/app/pages/pageNotFound/pageNotFound.component.ts
@@ -3,29 +3,23 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { EventData } from "tns-core-modules/ui/page/page";
 import { SelectedIndexChangedEventData, TabView } from "tns-core-modules/ui/tab-view";
 import { GameIDService } from "~/app/services/gameID.service";
 import { GamesEnum } from "~/app/shared/gamesEnum";
 
 @Component({
-    selector: "Leaderboard",
+    selector: "Page Not Found",
     moduleId: module.id,
-    templateUrl: "./leaderboard.component.html"
+    templateUrl: "./pageNotFound.component.html"
 })
-export class LeaderboardComponent implements OnInit {
+export class PageNotFoundComponent implements OnInit {
 
-    private gameSelection: GamesEnum = GamesEnum.ANGRY_CATS;
-
-    constructor(gameIDService: GameIDService) {
+    constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
-    }
-
-    onDrawerButtonTap(): void {
-        const sideDrawer = <RadSideDrawer>app.getRootView();
-        sideDrawer.showDrawer();
     }
 }
