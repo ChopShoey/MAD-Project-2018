8db1a26e24f6600f40c120ed0390cd07f69c7180
Lee Shuman <jls.engr@gmail.com>
2019-03-19 13:06:06 -0500

    Made stats persistent, fixed crashing bug.

--- MAD Game Collection/src/app/pages/arena/shared/gladiator.ts
+++ MAD Game Collection/src/app/pages/arena/shared/gladiator.ts
@@ -4,7 +4,7 @@
 import * as trace from "tns-core-modules/trace";
 import { Equipment } from "./equipment";
 import { GladiatorsEquipment } from "./gladiatorsEquipment";
-import { GladiatorStatistics } from "./gladiatorStatistic";
+import { GladiatorStatistics } from "./gladiatorStatistics";
 
 export class Gladiator {
     name: string;
@@ -12,7 +12,8 @@ export class Gladiator {
     gladiatorsEquipment: GladiatorsEquipment;
 
     // Imports the playerService so that the GUID can be validated before being set as PlayerInfo
-    constructor(name: string) {
+    constructor(name: string, gladiatorStatistics: GladiatorStatistics = new GladiatorStatistics()) {
         name === null || name.trim() !== "" ? this.name = name : trace.error("Name cannot be empty");
+        this.gladiatorStatistics = gladiatorStatistics;
     }
 }
